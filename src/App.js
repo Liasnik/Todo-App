4e3543f1c7f3f3939ce3a7ca5fa6d945b474652d
Alexandr <9426247@gmail.com>
2023-07-04 18:17:26 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import { useRef, useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import './App.css'
+import TodoForm from './components/Todos/TodoForm'
+import TodoList from './components/Todos/TodoList'
+import TodosActions from './components/Todos/TodosActions'
+
+function storageTodo() {
+  const savedTodos = localStorage.getItem('todos')
+  if (savedTodos) {
+    return JSON.parse(savedTodos)
+  }
+  return []
+}
+
+export default function App() {
+  const [todos, setTodos] = useState(storageTodo())
+  const downRef = useRef(null)
+
+  localStorage.setItem('todos', JSON.stringify(todos))
+
+  const addTodoHandler = (text) => {
+    const newTodo = {
+      id: uuidv4(),
+      text: text,
+      done: false,
+    }
+
+    setTodos([...todos, newTodo])
+  }
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id))
+  }
+
+  const doneToggleHendler = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, done: !todo.done } : { ...todo }
+      )
+    )
+  }
+
+  const resetTodos = () => setTodos([])
+
+  const clearCompleted = () => {
+    setTodos(todos.filter((todo) => !todo.done))
+  }
+
+  const scrollDown = () => {
+    downRef.current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'nearest',
+    })
+  }
+
+  const countDone = todos.filter((todo) => todo.done === true).length
+  return (
+    <div className="App">
+      <h1>todo list </h1>
+      <div className="header">
+        <TodoForm addTodo={addTodoHandler} scrollDown={scrollDown} />
+        {!!countDone && (
+          <h3>
+            Completed {countDone} {countDone > 1 ? 'todos' : 'todo'}
+          </h3>
+        )}
+      </div>
+      <div className="body">
+        <TodoList
+          todos={todos}
+          deleteTodo={deleteTodo}
+          doneHendler={doneToggleHendler}
+        />
+        {!!todos.length && (
+          <div ref={downRef}>
+            <TodosActions
+              resetTodos={resetTodos}
+              clearCompleted={clearCompleted}
+              todos={todos}
+              countDone={!!countDone} // convert to boolean
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Todos/TodoForm.js
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import styles from './TodoForm.module.css'
+import Button from '../UI/Button'
+
+export default function TodoForm({ addTodo, scrollDown }) {
+  const [text, setText] = useState('')
+
+  const addSubmitHandler = (e) => {
+    e.preventDefault()
+    addTodo(text)
+    setText('')
+  }
+
+  return (
+    <div className={styles.todoFormContainer}>
+      <form onSubmit={addSubmitHandler}>
+        <input
+          placeholder="Enter new todo"
+          onChange={(e) => setText(e.target.value)}
+          value={text}
+        />
+        <Button type="submit" title="Submit" onClick={scrollDown}>
+          Submit
+        </Button>
+      </form>
+    </div>
+  )
+}
+
+// return (
+//   <div className="">
+//     <form onChange={addTextHandler}>
+//       <input type="text" placeholder="Enter new todo" value={text} />
+//       <button type="reset" onClick={() => addTodo(text)}>
+//         Submit
+//       </button>
+//     </form>
+//   </div>
+// )
