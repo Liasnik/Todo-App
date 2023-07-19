0c129231a8d039b743aa73c741d61ee65b34af7f
Alexandr <9426247@gmail.com>
2023-07-19 15:58:03 +0300

    returned individual elements in Roures

--- src/App.js
+++ src/App.js
@@ -5,25 +5,23 @@ import NotFound from './components/NotFound'
 import MainLayout from './layouts/MainLayout'
 
 export default function App() {
-  const todoLists = ['1', '2', '3']
-
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
           <Route path="/" element={<MainLayout />}>
-            {todoLists.map((listName) => (
-              <Route
-                key={listName}
-                path={`/todoList${listName}`}
-                element={
-                  <TodoApp
-                    name={`todo list ${listName}`}
-                    listName={`todoList${listName}`}
-                  />
-                }
-              />
-            ))}
+            <Route
+              index
+              element={<TodoApp name="todo list 1" listName="todoList1" />}
+            />
+            <Route
+              path="todoList2"
+              element={<TodoApp name="todo list 2" listName="todoList2" />}
+            />
+            <Route
+              path="todoList3"
+              element={<TodoApp name="todo list 3" listName="todoList3" />}
+            />
             <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
